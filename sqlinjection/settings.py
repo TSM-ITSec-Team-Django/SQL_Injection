2d140f0d5379ed448ea1e5ff11a85854976e8df3
Samuel Helbling <samuel.helbling@icloud.com>
2021-11-09 22:46:07 +0100

    Added simple login which is using Django authentication system

--- sqlinjection/settings.py
+++ sqlinjection/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'sqlinjection.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
